612950788f14a9a6c180f1cf95d3ce8637d59532
jackalkao <shinjikao@gmail.com>
2017-05-30 22:04:08 +0800

    add base Auth

--- cleaners/views.py
+++ cleaners/views.py
@@ -3,9 +3,11 @@ from cleaners.models import Cleaner
 from cleaners.serializers import CleanerSerializer
 
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here.
 class CleanerViewSet(viewsets.ModelViewSet):
     queryset = Cleaner.objects.all()
     serializer_class = CleanerSerializer
+    permission_classes = (IsAuthenticated,)
